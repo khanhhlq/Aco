4fb527b7c56052643192baaf3757b2708b652378
ooAkira <lee.quockhanh1107@gmail.com>
2022-05-26 09:24:43 +0700

    Finish get avatar user ✳

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 // Require the necessary discord.js and dotenv classes
-const { Client, Intents, Presence } = require('discord.js')
+const { Client, Intents, Presence, MessageEmbed } = require('discord.js')
 require('dotenv').config();
 
 // Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
 client.on('ready', () => {
-    console.log("Bot is ready! ✅")
+    console.log("Bot cc đã sẵn sàng hoạt động ✅")
     client.user.setPresence(
         {
-            activities: [{ name: 'Aco'}], 
+            activities: [{ 
+                name: 'Chào cc nhé 🐞',
+                type: 'STREAMING'
+            }], 
             status: "dnd" 
         }
     )
@@ -22,11 +25,19 @@ client.on('messageCreate', message => {
     if (message.author.bot) return
     if (!message.content.startsWith(prefix)) return
     if (command === 'ping'){
-        message.reply(`**🏓 Pong!** ***Your ping is currently*** \`${client.ws.ping}ms\``)
+        message.reply(`**🏓 Pong cc!** ***Ping của mày hiện tại là*** \`${client.ws.ping}ms\``)
     } else if (command === 'say'){
         if (message.deletable) message.delete()
         message.channel.send(args.join(' '))
-    } 
+    } else if (command === 'avatar'){
+        const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.member
+        const avatarURL = member.displayAvatarURL({ format: 'png', size: 4096, dynamic: true})
+        const embed = new MessageEmbed()
+            .setImage(avatarURL)
+            .setTitle(`Avatar của thằng ${member.displayName} 👏`)
+        message.channel.send({ embeds: [embed]})
+    }   
+ 
 })
 
 client.login(process.env.TOKEN)
